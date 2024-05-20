b9012efc1d36b16868af9b44803dfec177d84293
eminbasbayan <emin.basbayan@icloud.com>
2024-05-20 15:48:58 +0300

    useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  const [fullName, setFullName] = useState("Ahmet");
+  // let fullName = "Ahmet"; // Mehmet
+
+  function handleChangeName() {
+    // fullName = "Mehmet";
+    // if (fullName === "Ahmet") {
+    //   setFullName("Mehmet");
+    // } else {
+    //   setFullName("Ahmet");
+    // }
+    
+    // ternary ?(true) :(false)
+    setFullName(fullName === "Ahmet" ? "Mehmet" : "Ahmet");
+    // console.log(fullName);
+  }
+
+  return (
+    <div className="app">
+      <p>{fullName}</p>
+      <button onClick={handleChangeName}>Click!</button>
+    </div>
+  );
 }
 
 export default App;
