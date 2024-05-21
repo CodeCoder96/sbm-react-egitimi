e4033d4cb31035e35517dcb75f37c26b891a0a0e
eminbasbayan <emin.basbayan@icloud.com>
2024-05-21 11:34:29 +0300

    dinamik prop alma

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,24 +5,15 @@ import "./Products.css";
 function Products() {
   return (
     <div className="products">
-      <ProductItem
-        image={productsData[0].image}
-        title={productsData[0].title}
-        price={productsData[0].price}
-        desc={productsData[0].description}
-      />
-      <ProductItem
-        image={productsData[1].image}
-        title={productsData[1].title}
-        price={productsData[1].price}
-        desc={productsData[1].description}
-      />
-      <ProductItem
-        image={productsData[2].image}
-        title={productsData[2].title}
-        price={productsData[2].price}
-        desc={productsData[2].description}
-      />
+      {productsData.map((product) => (
+        <ProductItem
+          key={product.id}
+          image={product.image}
+          title={product.title}
+          price={product.price}
+          desc={product.description}
+        />
+      ))}
     </div>
   );
 }
