761b16c4336843ba6240087df151d226f9faed04
eminbasbayan <emin.basbayan@icloud.com>
2024-05-21 14:57:43 +0300

    tek state ve tek function ile form yönetim

--- src/components/Products/AddNewProduct.jsx
+++ src/components/Products/AddNewProduct.jsx
@@ -3,33 +3,17 @@ import Button from "../UI/Button";
 import "./AddNewProduct.css";
 
 function AddNewProduct() {
-  const [title, setTitle] = useState("");
-  const [image, setImage] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState(0);
-
-  function handleTitleChange(event) {
-    setTitle(event.target.value);
-  }
-
-  function handleImageChange(event) {
-    setImage(event.target.value);
-  }
-
-  function handleDescriptionChange(event) {
-    setDescription(event.target.value);
-  }
-
-  function handlePriceChange(event) {
-    setPrice(event.target.value);
-  }
-
-  console.log({
-    title,
-    image,
-    description,
-    price,
+  const [productData, setProductData] = useState({
+    title: "",
+    image: "",
+    description: "",
+    price: "",
   });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setProductData((prevState) => ({ ...prevState, [name]: value }));
+  }
 
   return (
     <form className="product-form">
@@ -37,32 +21,36 @@ function AddNewProduct() {
         <label>Title</label>
         <input
           type="text"
-          onChange={handleTitleChange}
+          onChange={handleChange}
           placeholder="Ürün ismi giriniz."
+          name="title"
         />
       </div>
       <div className="product-input">
         <label>Image</label>
         <input
           type="text"
-          onChange={handleImageChange}
+          onChange={handleChange}
           placeholder="Ürün görseli giriniz."
+          name="image"
         />
       </div>
       <div className="product-input">
         <label>Description</label>
         <input
           type="text"
-          onChange={handleDescriptionChange}
+          onChange={handleChange}
           placeholder="Ürün açıklaması giriniz."
+          name="description"
         />
       </div>
       <div className="product-input">
         <label>Price</label>
         <input
           type="number"
-          onChange={handlePriceChange}
+          onChange={handleChange}
           placeholder="Ürün fiyatı giriniz."
+          name="price"
         />
       </div>
       <Button>Yeni Ürün Ekle</Button>
