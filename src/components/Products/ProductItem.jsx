8c6755c99c3e9cf4683bc48f99851ea4b4e93d93
eminbasbayan <emin.basbayan@icloud.com>
2024-05-21 10:23:54 +0300

    component içinde javascript yazma

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -1,18 +1,22 @@
 import "./ProductItem.css";
 
 function ProductItem() {
+  const image = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg";
+  const title = "Çanta";
+  const price = 500;
+  
   return (
     <div className="product-item">
       <div className="product-image">
         <img
-          src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+          src={image}
           width={200}
           alt="product image"
         />
       </div>
       <div className="product-info">
-        <strong>Title</strong>
-        <span>500₺</span>
+        <strong>{title}</strong>
+        <span>{price}₺</span>
       </div>
     </div>
   );
