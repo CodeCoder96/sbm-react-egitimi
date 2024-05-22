b5515f7f6d8c755088d0070c2706a45f9dfcb00b
eminbasbayan <emin.basbayan@icloud.com>
2024-05-22 16:27:13 +0300

    NavLink component

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   const { cartItems } = useContext(CartContext);
@@ -18,24 +18,29 @@ const Header = () => {
           <div className="collapse navbar-collapse">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link to="/" className="nav-link active">
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active text-danger" : "nav-link"
+                  }
+                >
                   Home
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link to="/products" className="nav-link">
+                <NavLink to="/products" className="nav-link">
                   Products
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link to="/about" className="nav-link">
+                <NavLink to="/about" className="nav-link">
                   About
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link to="/cart" className="nav-link">
+                <NavLink to="/cart" className="nav-link">
                   Cart
-                </Link>
+                </NavLink>
               </li>
             </ul>
             <div className="d-flex align-items-center">
