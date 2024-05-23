ca39da14dc44ba51bb1c9ad24236d0a001478f69
eminbasbayan <emin.basbayan@icloud.com>
2024-05-23 15:06:00 +0300

    useDispatch hook kullanımı

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,15 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { arttir, azalt } from "../redux/counterSlice";
 
 function Counter() {
-  const state = useSelector((state)=> state.counter);
+  const state = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <div className="counter">
-      <button onClick={()=> {}}>+</button>
+      <button onClick={() => dispatch(arttir())}>+</button>
       <strong>{state.count}</strong>
-      <button onClick={()=> {}}>-</button>
+      <button onClick={() => dispatch(azalt())}>-</button>
     </div>
   );
 }
